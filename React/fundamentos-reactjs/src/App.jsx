a02db72e0da28508164d9968570adbfafe5c08d2
Felipe Amaral <felipeamaraldev15@gmail.com>
2023-02-04 23:44:37 -0300

    css modules

--- React/fundamentos-reactjs/src/App.jsx
+++ React/fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,13 @@
 import {Post} from "./Post"
+import { Header } from "./components/Header"
+
+import './styles.css'
 
 function App() {
 
   return (
     <>
+      <Header />
       <Post author= "Felipe" content="estudo sobre propriedades"/>
       <Post author="Tufão" content="gato dorminhoco"/>
     </>  
